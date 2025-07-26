88bef72e5eff2710bb732370b706ae950dc1991c
hyewondo <hw.do@dohands.com>
2025-07-26 15:14:54 +0900

    fix: 배포 정보 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { TestProvider } from './context/TestContext';
 import HomePage from './components/HomePage';
 import TestPage from './components/TestPage';
